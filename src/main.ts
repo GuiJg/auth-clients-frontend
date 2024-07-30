1a2d0d047544f4030a099ef3cf4b1b04a2d79a93
GuiJg <j.gu1lh3rm325@gmail.com>
2024-07-29 22:02:01 -0300

    fix: mudando a rota principal para /clientes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './app/home/home.component';
+import { DataFormComponent } from './app/data-form/data-form.component';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter([
+      { path: '', redirectTo: '/clientes', pathMatch: 'full' },
+      { path: 'clientes', component: HomeComponent },
+      { path: 'cadastrar', component: DataFormComponent },
+    ], withComponentInputBinding()),
+    importProvidersFrom(
+      BrowserAnimationsModule,
+      ToastrModule.forRoot()
+    ),
+    provideHttpClient()
+  ]
+}).catch(err => console.error(err));
