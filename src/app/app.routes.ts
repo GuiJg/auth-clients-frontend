dfbcea37ad44ddcdf888b912d6bd3652fe9038b3
GuiJg <j.gu1lh3rm325@gmail.com>
2024-07-29 22:03:35 -0300

    fix: mudando a rota principal para /clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DataFormComponent } from './data-form/data-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/clientes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'cadastrar',
+    component: DataFormComponent
+  },
+  {
+    path: 'clientes',
+    component: HomeComponent
+  }
+];
